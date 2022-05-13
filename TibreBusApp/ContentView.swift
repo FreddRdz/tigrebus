9babc20b7277ab6d760bc59897429d3cfed02ad5
Alfredo Rodríguez <fredd.073100@gmail.com>
2022-05-12 19:53:23 -0500

    Initial Commit

---
+++ TibreBusApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TibreBusApp
+//
+//  Created by Alfredo Rodriguez Coronado on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TibreBusApp/TibreBusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TibreBusAppApp.swift
+//  TibreBusApp
+//
+//  Created by Alfredo Rodriguez Coronado on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TibreBusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
